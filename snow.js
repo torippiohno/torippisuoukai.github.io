9386259d5da251510aca630983f93ee1f03050ba
torippiohno <kotaronika1221@gmail.com>
2025-01-30 13:01:35 +0900

    Create snow.js

---
+++ snow.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    const snow = document.createElement('span');
+    snow.classList.add('snow');
+
+    snow.style.width = snow.style.height = Math.random() * 8 + 'px'; 
+    snow.style.left = Math.random() * 100 + '%'; 
+
+    document.querySelector('.snow-container').appendChild(snow);
+
+    setTimeout(() => {
+        snow.remove();
+    }, 10000 );
+}, 100
